d7a46a984063dd4596f95cd0c823e2ee71315bd8
shaansen <shantanu.sengupta2008@gmail.com>
2020-09-05 15:46:54 -0700

    Style improvements - responsive

--- client/components/AddEvent.jsx
+++ client/components/AddEvent.jsx
@@ -22,6 +22,7 @@ const AddEvent = () => {
   const [complete, setComplete] = useState(false);
   const [dtp, setDTP] = useState(new Date());
   const [saveddtp, setsavedDTP] = useState(null);
+  const [later, setLater] = useState(false);
   const onChange = (date) => setDTP(date);
 
   //   useEffect(() => {
@@ -36,7 +37,7 @@ const AddEvent = () => {
   //   }, [])
 
   return (
-    <div className="report-event-container">
+    <React.Fragment>
       <div className="report-created-alert">
         {complete && (
           <Alert severity="success">
@@ -46,61 +47,72 @@ const AddEvent = () => {
           </Alert>
         )}
       </div>
-
-      <div className="report-event-now">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={(e) => {
-            setComplete(false);
-            e.preventDefault();
-            const d = new Date();
-            addEvent({
-              variables: { date: d.toString() },
-              refetchQueries: [{ query: GET_EVENTS }],
-            });
-            setsavedDTP(d);
-            setComplete(true);
-          }}
-        >
-          Report Epilepsy Now
-        </Button>
+      <div className="report-event-container">
+        {!later ? (
+          <div className="report-event-now">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(e) => {
+                setComplete(false);
+                e.preventDefault();
+                const d = new Date();
+                addEvent({
+                  variables: { date: d.toString() },
+                  refetchQueries: [{ query: GET_EVENTS }],
+                });
+                setsavedDTP(d);
+                setComplete(true);
+              }}
+            >
+              Report Epilepsy Now
+            </Button>
+            <Button onClick={() => setLater(true)}>
+              Report event at custom time instead
+            </Button>
+          </div>
+        ) : (
+          <div className="report-event-custom">
+            <h4>Report at custom time</h4>
+            {/* <Datetime value={dtp} defaultValue={dtp} onChange={onChange} /> */}
+            <TextField
+              id="datetime-local"
+              type="datetime-local"
+              defaultValue={
+                moment(dtp).format("YYYY-MM-DD") +
+                "T" +
+                moment(dtp).format("HH:mm")
+              }
+              onChange={(e) =>
+                onChange(moment(e.target.value, "YYYY-MM-DDTHH:mm"))
+              }
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(e) => {
+                setComplete(false);
+                e.preventDefault();
+                addEvent({
+                  variables: { date: dtp },
+                  refetchQueries: [{ query: GET_EVENTS }],
+                });
+                setsavedDTP(dtp);
+                setComplete(true);
+              }}
+            >
+              Report Epilepsy Event
+            </Button>
+            <Button onClick={() => setLater(false)}>
+              Report event now instead
+            </Button>
+          </div>
+        )}
       </div>
-      <hr />
-      <h4 className="or">OR</h4>
-      <hr />
-      <div className="report-event-custom">
-        <h4>Report at custom time</h4>
-        {/* <Datetime value={dtp} defaultValue={dtp} onChange={onChange} /> */}
-        <TextField
-          id="datetime-local"
-          type="datetime-local"
-          defaultValue={
-            moment(dtp).format("YYYY-MM-DD") + "T" + moment(dtp).format("HH:mm")
-          }
-          onChange={(e) => onChange(moment(e.target.value, "YYYY-MM-DDTHH:mm"))}
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={(e) => {
-            setComplete(false);
-            e.preventDefault();
-            addEvent({
-              variables: { date: dtp },
-              refetchQueries: [{ query: GET_EVENTS }],
-            });
-            setsavedDTP(dtp);
-            setComplete(true);
-          }}
-        >
-          Report Epilepsy Event
-        </Button>
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
