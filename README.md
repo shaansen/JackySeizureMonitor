1a2204410cf34703cbbca2d9a6dea61531a3cee7
shaansen <shantanu.sengupta2008@gmail.com>
2020-09-05 20:40:08 -0700

    Final Deployment

--- README.md
+++
@@ -1,3 +0,0 @@
-# Lyrical-GraphQL
-
-Starter project from a GraphQL course on Udemy.com
--- server/models/event.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const EventSchema = new Schema({
-  date: { type: String },
-});
-
-mongoose.model("event", EventSchema);
