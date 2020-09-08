73f7f5295ddba83e80a90b0d079ddfc4f8bd6813
shaansen <shantanu.sengupta2008@gmail.com>
2020-09-07 20:45:04 -0700

    Auth routing setup

--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,2 @@
 require("./event");
+require("./user");
---
+++ server/services/passport.js
@@ -0,0 +1,35 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleID: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = new User({ googleID: profile.id });
+        const newUser = await user.save();
+        done(null, newUser);
+      }
+    }
+  )
+);
