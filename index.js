f36d59bbcfedf3650be6aa8c008a3c7f0f0051a3
shaansen <shantanu.sengupta2008@gmail.com>
2020-09-07 20:55:05 -0700

    Test deployment

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
-const passport = require("passport");
-const keys = require("./config/keys");
-require("./models/user");
-require("./services/passport");
+// const mongoose = require("mongoose");
+// const cookieSession = require("cookie-session");
+// const passport = require("passport");
+// const keys = require("./config/keys");
+// require("./models/user");
+// require("./services/passport");
 
-mongoose
-  .connect(keys.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log(`MongoDB Connected…`);
-  })
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(keys.mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log(`MongoDB Connected…`);
+//   })
+//   .catch((err) => console.log(err));
 
 const app = express();
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
-  })
-);
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [keys.cookieKey],
+//   })
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-const authRoutes = require("./routes/authRoutes");
-authRoutes(app);
+// const authRoutes = require("./routes/authRoutes");
+// authRoutes(app);
+
+app.get("/", (req, res) => {
+  res.send({ test: "gasdasda" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
