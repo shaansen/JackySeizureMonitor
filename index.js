b25af4cc123635963d2ab4b00a392e182d110d06
shaansen <shantanu.sengupta2008@gmail.com>
2020-09-15 18:52:10 -0700

    Fix express routing for client

--- index.js
+++ index.js
@@ -31,9 +31,13 @@ app.use(passport.session());
 const authRoutes = require("./routes/authRoutes");
 authRoutes(app);
 
-app.get("/", (req, res) => {
-  res.send({ test: "gasdasda" });
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
